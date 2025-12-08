a2b5b1da62b36ca47fc5e5b743f6cfdf0ecdbaf1
MJ <manoj.kumar31028@gmail.com>
2025-12-07 23:15:06 -0500

    Update utils.py

--- retsulc/utils.py
+++ retsulc/utils.py
@@ -15,3 +15,25 @@ for idx, row in pred_cluster_counts_df.iterrows():
     stats[current_category]["Clusters with SameMatchGroupIds (TP)"] += 1
   else:
     stats[current_category]["Clusters with DiffMatchGroupIds (FN)"] += 1
+
+
+### LCS 
+
+def lcs_len(a, b):
+    m, n = len(a), len(b)
+    dp = [[0]*(n+1) for _ in range(m+1)]
+
+    for i in range(m):
+        for j in range(n):
+            if a[i] == b[j]:
+                dp[i+1][j+1] = dp[i][j] + 1
+            else:
+                dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+    return dp[m][n]
+
+for col1, col2 in combinations(char_fields, 2):
+    df[f"{col1}_{col2}"] = df.apply(
+        lambda r: lcs_len(str(r[col1]), str(r[col2])),
+        axis=1
+    )
+
