5efda23b349edef1bbc21ef51cd4795a3cd3bd94
MJ <manoj.kumar31028@gmail.com>
2025-12-19 00:08:01 -0500

    Update utils.py

--- retsulc/utils.py
+++ retsulc/utils.py
@@ -109,4 +109,17 @@ df[char_fields] = (
 # MatchGroup size
 df["matchgroup_size"] = df.groupby("MatchGroupId")["MatchGroupId"].transform("size")
 
+def lcs_continuous_len(a, b):
+    m, n = len(a), len(b)
+    dp = [[0]*(n+1) for _ in range(m+1)]
+    longest = 0
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+                longest = max(longest, dp[i][j])
+    return longest
+
+
 
